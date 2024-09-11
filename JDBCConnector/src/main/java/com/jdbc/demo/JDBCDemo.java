0189e13f196ea66889c36c451b4c7611296109de
kavyasri <kavyasri@607@gmail.com>
2024-09-11 19:03:18 +0530

    Added jdbc, notes

---
+++ JDBCConnector/src/main/java/com/jdbc/demo/JDBCDemo.java
@@ -0,0 +1,32 @@
+package com.jdbc.demo;
+
+
+import java.sql.*;
+
+public class JDBCDemo {
+    public static void main(String[] args) {
+
+        String query = "select * from college.persondetails";
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/college?user=root&password=Password!123");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+//        System.out.println(resultSet);
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getInt(3) + " " +
+                        resultSet.getBoolean(4));
+            }
+
+            preparedStatement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
